19681fbb6b83654e4b185d8195f79a19517f2ed9
Epaminond <k4merton@gmail.com>
2015-05-22 17:06:30 +0300

    Update nodemailer version

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
   name: 'epaminond:nodemailer',
-  version: '1.3.2',
+  version: '1.3.4',
   summary: "Meteor wrapper for Nodemailer",
   git: 'https://github.com/epaminond/meteor-nodemailer.git',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'nodemailer': '1.3.2'
+  'nodemailer': '1.3.4'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.2');
+  api.versionsFrom('1.0.3.4');
   api.add_files('nodemailer.js', 'server');
   api.export("Nodemailer");
 });
